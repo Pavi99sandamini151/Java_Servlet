6f4f3c5bfd365e3016085a2ae3ad080d27abf60e
Pavithra Sandamini <84126210+Pavi99sandamini151@users.noreply.github.com>
2023-01-21 07:46:23 +0530

    Servlet session management implemented

---
+++ servlet_session_management/src/main/java/com/FirstServlet.java
@@ -0,0 +1,73 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import dto.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		User u = new User();
+		HttpSession session =request.getSession();
+		
+		u.setFname(request.getParameter("fname"));
+		u.setLname(request.getParameter("lname"));
+		session.setAttribute("user", u);
+		response.sendRedirect("two.html");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
